b3fb52331acfad1e57c6487c5915160c268c6daa
pilcrow <mjp@pilcrow.madison.wi.us>
2011-03-10 22:20:17 -0600

    Log errors in fopen()/connect() functions, document SCConfLogOpenGeneric.
    * src/util-logopenfile.c
      SCConfLogOpenGeneric: Add documentation.  Check args for sanity and revert
      to already avaiable YAML config options.  Log success and carp on invalid
      entries for 'type'
    
      SCLogOpenFileFp: Add documentation, log errors.
    
      SCLogOpenSocketFp: Add documentation, log errors.  Don't bother preserving
      errno.

--- src/util-logopenfile.c
+++ src/util-logopenfile.c
@@ -4,21 +4,28 @@
 #include <sys/un.h>
 
 #include "suricata-common.h" /* errno.h, string.h, etc. */
-#include "conf.h"            /* ConfNode, etc. */
 #include "tm-modules.h"      /* LogFileCtx */
+#include "conf.h"            /* ConfNode, etc. */
+#include "output.h"          /* DEFAULT_LOG_* */
 
+/** \brief connect to the indicated local stream socket, logging any errors
+ *  \param path filesystem path to connect to
+ *  \retval FILE* on success (fdopen'd wrapper of underlying socket)
+ *  \retval NULL on error
+ */
 FILE * SCLogOpenSocketFp(const char *path)
 {
-    struct sockaddr_un sun = { AF_UNIX };
+    struct sockaddr_un sun;
     FILE * ret;
     int s = -1;
 
     s = socket(PF_UNIX, SOCK_STREAM, 0);
     if (s < 0) goto err;
 
+    sun.sun_family = AF_UNIX;
     strncpy(sun.sun_path, path, sizeof(sun.sun_path) - 1);
 
-    if (connect(s, &sun, sizeof(sun)) < 0)
+    if (connect(s, (const struct sockaddr *)&sun, sizeof(sun)) < 0)
         goto err;
 
     ret = fdopen(s, "w");
@@ -28,59 +35,101 @@ FILE * SCLogOpenSocketFp(const char *path)
     return ret;
 
 err:
-    if (s >= 0) {
-        int _errno = errno; /* Hey, you never know. */
+    SCLogError(SC_ERR_SOCKET, "Error connecting to socket \"%s\": %s",
+               path, strerror(errno));
+
+    if (s >= 0)
         close(s);
-        errno = _errno;
-    }
+
     return NULL;
 }
 
-FILE * SCLogOpenFileFp(const char *path, const char *mode)
+/** \brief open the indicated file, logging any errors
+ *  \param path filesystem path to open
+ *  \param append_setting open file with O_APPEND: "yes" or "no"
+ *  \retval FILE* on success
+ *  \retval NULL on error
+ */
+FILE * SCLogOpenFileFp(const char *path, const char *append_setting)
 {
     FILE *ret = NULL;
 
-    if (strcasecmp(mode, "yes") == 0) {
-        ret = fopen(log_path, "a");
+    if (strcasecmp(append_setting, "yes") == 0) {
+        ret = fopen(path, "a");
     } else {
-        ret = fopen(log_path, "w");
+        ret = fopen(path, "w");
     }
 
+    if (ret == NULL)
+        SCLogError(SC_ERR_FOPEN, "Error opening file: \"%s\": %s",
+                   path, strerror(errno));
     return ret;
 }
 
-int SCConfGenericOutput(ConfNode *conf,
-                        LogFileCtx *file_ctx,
-                        const char *default_filename)
+/** \brief open a generic output "log file", which may be a regular file or a socket
+ *  \param conf ConfNode structure for the output section in question
+ *  \param log_ctx Log file context allocated by caller
+ *  \param default_filename Default name of file to open, if not specified in ConfNode
+ *  \retval 0 on success
+ *  \retval -1 on error
+ */
+int SCConfLogOpenGeneric(ConfNode *conf,
+                         LogFileCtx *log_ctx,
+                         const char *default_filename)
 {
     char log_path[PATH_MAX];
-    char *filename, *filetype, *log_dir;
+    char *log_dir;
+    const char *filename, *filetype;
 
-    filename = ConfNodeLookupChildValueDef(conf, "filename",
-                                           default_filename);
-    filetype = ConfNodeLookupChildValueDef(conf, "type",
-                                           DEFAULT_LOG_FILETYPE);
-    ConfGetDef("default-log-dir", &log_dir, DEFAULT_LOG_DIR);
-
-    snprintf(log_path, PATH_MAX, "%s/%s", log_dir, filename);
-
-    if (strcasecmp(filetype, "socket") == 0) {
-        log_ctx->fp = SCLogOpenSocketFp(log_path);
-    } else if (strcasecmp(filetype, DEFAULT_LOG_FILETYPE)) {
-        const char *append;
-
-        append = ConfNodeLookupChildValueDef(conf, "append",
-                                             DEFAULT_LOG_MODE_APPEND);
-        log_ctx = SCLogOpenFileFp(log_path, append);
-    } else {
-        SCLogError(SC_ERR_CONF, "%s: unrecognized file \"type\" \"%s\"",
-                   conf->name, filetype);
+    // Arg check
+    if (conf == NULL || log_ctx == NULL || default_filename == NULL) {
+        SCLogError(SC_ERR_INVALID_ARGUMENT,
+                   "SCConfLogOpenGeneric(conf %p, ctx %p, default %p) "
+                   "missing an argument",
+                   conf, log_ctx, default_filename);
+        return -1;
+    }
+    if (log_ctx->fp != NULL) {
+        SCLogError(SC_ERR_INVALID_ARGUMENT,
+                   "SCConfLogOpenGeneric: previously initialized Log CTX "
+                   "encountered");
         return -1;
     }
 
-    if (log_ctx->fp == NULL)
-        SCLogError(SC_ERR_CONF, "%s: error on output \"%s\": %s",
-                   conf->name, filename, strerror(errno));
+    // Resolve the given config
+    filename = ConfNodeLookupChildValue(conf, "filename");
+    if (filename == NULL)
+        filename = default_filename;
 
-    return log_ctx;
+    if (ConfGet("default-log-dir", &log_dir) != 1)
+        log_dir = DEFAULT_LOG_DIR;
+
+    snprintf(log_path, PATH_MAX, "%s/%s", log_dir, filename);
+
+    filetype = ConfNodeLookupChildValue(conf, "type");
+    if (filetype == NULL)
+        filetype = DEFAULT_LOG_FILETYPE;
+
+    // Now, what have we been asked to open?
+    if (strcasecmp(filetype, "socket") == 0) {
+        log_ctx->fp = SCLogOpenSocketFp(log_path);
+    } else if (strcasecmp(filetype, DEFAULT_LOG_FILETYPE) == 0) {
+        const char *append;
+
+        append = ConfNodeLookupChildValue(conf, "append");
+        if (append == NULL)
+            append = DEFAULT_LOG_MODE_APPEND;
+        log_ctx->fp = SCLogOpenFileFp(log_path, append);
+    } else {
+        SCLogError(SC_ERR_INVALID_YAML_CONF_ENTRY, "Invalid entry for "
+                   "%s.type.  Expected \"regular\" (default) or \"socket\"",
+                   conf->name);
+    }
+
+    if (log_ctx->fp == NULL)
+        return -1; // Error already logged by Open...Fp routine
+
+    SCLogInfo("%s output initialized: %s", conf->name, filename);
+
+    return 0;
 }
